e6b854aab904cb32c78f5d6f2bb101cf21fcd49f
jamalhussien <jamalabuelhamd@gmail.com>
2026-03-10 02:29:10 +0200

    Remove unrefactored legacy files

--- api/__init__.py
+++
@@ -1,4 +0,0 @@
-from api.controller import TTSController
-from api.models import SynthesizeRequest
-
-__all__ = ["SynthesizeRequest", "TTSController"]
--- api/models.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class SynthesizeRequest(BaseModel):
-    text: str
